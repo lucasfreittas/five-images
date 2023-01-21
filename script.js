1419e6ab1bd651c2dd972bb1710a63de55c0721c
Lucas <l.freittas@outlook.com>
2023-01-21 12:13:20 -0300

    Mobile Test

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const cards = document.querySelectorAll('.card');
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        
-        removeClass()
-        card.classList.add('active')
-
-    })
-})
+cards.forEach(card => card.addEventListener('click', () => {
+    removeClass();
+    card.classList.add('active')
+}))
 
 function removeClass(){
     cards.forEach(card => {
